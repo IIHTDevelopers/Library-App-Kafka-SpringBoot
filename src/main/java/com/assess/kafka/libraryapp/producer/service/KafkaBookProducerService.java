caed860d6544bd5f682c8b9ff05e894267ef8cbf
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-12 10:50:30 +0530

    Add files via upload

---
+++ src/main/java/com/assess/kafka/libraryapp/producer/service/KafkaBookProducerService.java
@@ -0,0 +1,16 @@
+package com.assess.kafka.libraryapp.producer.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.assess.kafka.libraryapp.model.Book;
+
+@Service
+public class KafkaBookProducerService {
+    private KafkaTemplate<String, Book> kafkaTemplate;
+
+    
+    public void sendBook(String topic, Book book) {
+        
+    }
+}
